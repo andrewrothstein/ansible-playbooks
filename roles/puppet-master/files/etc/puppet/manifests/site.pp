e53b07e3de3d99d4cc1532faaa15ea5cf0881c3b
Andrew Rothstein <arothste@blackrock.com>
2015-07-07 14:21:31 -0700

    puppet config for ntp and the start of zookeeper

---
+++ roles/puppet-master/files/etc/puppet/manifests/site.pp
@@ -0,0 +1,101 @@
+node 'labccld001' {
+  class { "ntp":
+    servers    => ['0.us.pool.ntp.org iburst',
+                   '1.us.pool.ntp.org iburst',
+                   '2.us.pool.ntp.org iburst'],
+    autoupdate => false,
+    restrict   => [],
+    service_enable     => true,
+  }
+
+  class { 'zookeeper':
+    id => 1,
+    servers => ['45.32.103.20',
+                '45.32.103.21',
+                '45.32.103.22',
+                '45.32.103.23',
+                '45.32.103.24'],
+    install_java => true,
+    java_package => 'java-1.8.0-openjdk-headless'
+  }
+}
+
+node 'labccld002' {
+  class { "ntp":
+    servers    => [ 'labccld001' ],
+    autoupdate => true,
+    restrict   => [],
+    service_enable     => true,
+  }
+
+  class { 'zookeeper':
+    id => 2,
+    servers => ['45.32.103.20',
+                '45.32.103.21',
+                '45.32.103.22',
+                '45.32.103.23',
+                '45.32.103.24'],
+    install_java => true,
+    java_package => 'java-1.8.0-openjdk-headless'
+  }
+}
+
+node 'labccld003' {
+  class { "ntp":
+    servers    => [ 'labccld001' ],
+    autoupdate => true,
+    restrict   => [],
+    service_enable     => true,
+  }
+
+  class { 'zookeeper':
+    id => 3,
+    servers => ['45.32.103.20',
+                '45.32.103.21',
+                '45.32.103.22',
+                '45.32.103.23',
+                '45.32.103.24'],
+    install_java => true,
+    java_package => 'java-1.8.0-openjdk-headless'
+  }
+}
+
+node 'labccld004' {
+  class { "ntp":
+    servers    => [ 'labccld001' ],
+    autoupdate => true,
+    restrict   => [],
+    service_enable     => true,
+  }
+
+  class { 'zookeeper':
+    id => 4,
+    servers => ['45.32.103.20',
+                '45.32.103.21',
+                '45.32.103.22',
+                '45.32.103.23',
+                '45.32.103.24'],
+    install_java => true,
+    java_package => 'java-1.8.0-openjdk-headless'
+  }
+}
+
+node 'labccld005' {
+  class { "ntp":
+    servers    => [ 'labccld001' ],
+    autoupdate => true,
+    restrict   => [],
+    service_enable     => true,
+  }
+
+  class { 'zookeeper':
+    id => 5,
+    servers => ['45.32.103.20',
+                '45.32.103.21',
+                '45.32.103.22',
+                '45.32.103.23',
+                '45.32.103.24'],
+    install_java => true,
+    java_package => 'java-1.8.0-openjdk-headless'
+  }
+}
