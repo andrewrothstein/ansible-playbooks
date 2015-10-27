745368b1ad9a049887a9c0764303a304552d5955
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-10-27 14:46:06 -0400

    code for chopping roles and linking with submodules

--- ansible-subprojects.py
+++ ansible-subprojects.py
@@ -3,14 +3,14 @@
 import os
 from subprocess import call
 
-ansible_playbooks_roles_dir = "/Users/drew/git/github/andrewrothstein/ansible-playbooks/roles"
-github_repos_dir = "/Users/drew/git/github-target"
+ansible_playbooks_dir = "/Users/drew/git/github/andrewrothstein/ansible-playbooks"
+ansible_playbooks_roles_dir = ansible_playbooks_dir + "/roles"
 license_path = "/Users/drew/git/github/andrewrothstein/ansible-julia/LICENSE"
 readme_path = "/Users/drew/git/github/andrewrothstein/ansible-julia/README.md"
 
-d
-
 def create_git_repo(role) :
+    github_repos_dir = "/Users/drew/git/github-target"
+
     src_role_path = ansible_playbooks_roles_dir + "/" + role
     target_role_path = github_repos_dir + "/ansible-" + role
 
@@ -30,7 +30,20 @@ def create_git_repo(role) :
         call(['hub', 'create', 'ansible-' + role])
         call(['git', 'push', 'origin', 'master'])
 
-skip = set(['julia', 'proxify'])
+def chop_role(role) :
+    role_path = 'roles/' + role
+    os.chdir(ansible_playbooks_dir)
+    call(['git', 'rm', '-rf', role_path])
+    call(['git', 'commit', '-a', '-m', 'migrating to galaxified role ansible-' + role])
+    call(['git', 'submodule', 'add', 'git://github.com/andrewrothstein/ansible-' + role, role_path])
+    call(['git', 'commit', '-a', '-m', 'add submodule for ansible-' + role])
+    
+        
 
-roles = filter(lambda x : os.path.isdir(x) and x not in skip, os.listdir(ansible_playbooks_roles_dir))
-map(create_git_repo, roles)
+all_roles = filter(lambda x : os.path.isdir(x), os.listdir(ansible_playbooks_roles_dir))
+
+skip = set(['julia', 'proxify'])
+roles = filter(lambda x : x not in skip, all_roles)
+
+#map(create_git_repo, roles)
+map(chop_role, roles)
