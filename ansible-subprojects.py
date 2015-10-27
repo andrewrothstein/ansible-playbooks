2068891af11aacecdd0026faf6030a4ff1dfa409
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-10-27 14:51:42 -0400

    refined code for carving out submodules

--- ansible-subprojects.py
+++ ansible-subprojects.py
@@ -32,18 +32,24 @@ def create_git_repo(role) :
 
 def chop_role(role) :
     role_path = 'roles/' + role
+    github_remote = 'git://github.com/andrewrothstein/ansible-' + role + '.git'
+    print("chopping {0} from {1} and linking to {2}...".format(role_path, ansible_playbooks_dir, github_remote))
+
     os.chdir(ansible_playbooks_dir)
     call(['git', 'rm', '-rf', role_path])
     call(['git', 'commit', '-a', '-m', 'migrating to galaxified role ansible-' + role])
-    call(['git', 'submodule', 'add', 'git://github.com/andrewrothstein/ansible-' + role, role_path])
+    call(['git', 'submodule', 'add', github_remote, role_path])
     call(['git', 'commit', '-a', '-m', 'add submodule for ansible-' + role])
     
         
 
-all_roles = filter(lambda x : os.path.isdir(x), os.listdir(ansible_playbooks_roles_dir))
+all_roles = filter(lambda x : not os.path.isdir(x), os.listdir(ansible_playbooks_roles_dir))
 
 skip = set(['julia', 'proxify'])
 roles = filter(lambda x : x not in skip, all_roles)
 
 #map(create_git_repo, roles)
+print("all roles in {0}:".format(ansible_playbooks_roles_dir), all_roles)
+print("roles:", roles)
+
 map(chop_role, roles)
