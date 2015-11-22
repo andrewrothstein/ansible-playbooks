91676ce80bc42e57838e62c208e0a187e097ef44
Andrew Rothstein <andrew.rothstein@gmail.com>
2015-11-21 22:25:39 -0500

    script to help me migrate my roles out of a single playbook with ansible-galaxy

---
+++ galaxify.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+GIT_ROOT=~/git/github/andrewrothstein
+SRC=$GIT_ROOT/ansible-julia
+DEST=$GIT_ROOT/$1
+
+echo '**~' > $DEST/.gitignore
+mkdir -p $DEST/{defaults,meta}
+cp $SRC/meta/main.yml $DEST/meta/main.yml
+cp $SRC/test.yml $DEST/test.yml
